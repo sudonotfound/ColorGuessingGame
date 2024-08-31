b07c24648c677195e7015a2a951cbc9157fdfb6d
sudonotfound <103552057+sudonotfound@users.noreply.github.com>
2024-08-31 14:27:57 +0900

    ランダムの値をバックで確認できるように変更

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -13,8 +13,22 @@ class GameController < ApplicationController
     @b = params[:b].to_i
 
 
+    @randam_num=generate_new_color()
 
-    @score = calculate_score(@r, @g, @b)#インスタンスを呼び出している
+    
+    puts @randam_num
+    puts @randam_num[:r]
+    puts @randam_num[:g]
+    puts @randam_num[:b]
+    
+
+    @ran_r=@randam_num[:r]
+    @ran_g=@randam_num[:g]
+    @ran_b=@randam_num[:b]
+
+    
+    @score = calculate_score(@r, @g, @b,@ran_r,@ran_g,@ran_b)#インスタンスを呼び出している
+    
     @message = score_message(@score)#インスタンスを呼び出している
     @color = session[:color]#セッションから正解の色 (session[:color])を取得
     @correct_color = session[:color] # 正解の色をビューに渡す
@@ -45,20 +59,32 @@ class GameController < ApplicationController
   def generate_new_color
     @color = { r: rand(256), g: rand(256), b: rand(256) }
     session[:color] = @color
+    #puts @color
   end
 
-  def calculate_score(r, g, b)#RGB値の違いに基づいてスコアを計算する
+  def calculate_score(r, g, b,ran_r,ran_g,ran_b)#RGB値の違いに基づいてスコアを計算する
     correct_color = session[:color]
-    if correct_color.nil?
-      generate_new_color
-      correct_color = session[:color]
-    end
-    score = 0
-    score += point_for_color(r, correct_color[:r])
-    score += point_for_color(g, correct_color[:g])
-    score += point_for_color(b, correct_color[:b])
-    session[:score] += score
-    score
+        if r==ran_r && g==ran_g && b== ran_b then
+          puts(r)
+          puts(g)
+          puts(b)
+          puts("正解")
+        else
+          puts(r)
+          puts(g)
+          puts(b)
+          puts("間違い")  
+        end
+    #  if correct_color.nil?
+    #   generate_new_color
+    #   correct_color = session[:color]
+    # end
+    # score = 0
+    # score += point_for_color(r, correct_color[:r])
+    # score += point_for_color(g, correct_color[:g])
+    # score += point_for_color(b, correct_color[:b])
+    # session[:score] += score
+    # score
   end
 
   def point_for_color(guess, correct)
